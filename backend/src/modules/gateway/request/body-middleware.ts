6e252f3aab6da52b8d43cef23147dc45a67240ac
胡安民 <3426154361@qq.com>
2026-07-12 04:55:34 +0800

    feat: gate speed-first bodies before buffering

--- backend/src/modules/gateway/request/body-middleware.ts
+++ backend/src/modules/gateway/request/body-middleware.ts
@@ -34,7 +34,7 @@ import { extractClientIp, requestEndpoint } from './metadata.js'
 import { buildGatewayUsageContext } from './preflight.js'
 
 type GatewayRawBodyLimitScope = 'gateway' | 'text' | 'image'
-type GatewayBodyRejectReason = 'gateway_body_parser' | 'gateway_body_size_limit' | 'gateway_body_in_flight_limit' | 'gateway_body_metadata_worker'
+type GatewayBodyRejectReason = 'gateway_body_parser' | 'gateway_body_size_limit' | 'gateway_body_in_flight_limit' | 'gateway_body_metadata_worker' | 'gateway_body_admission'
 
 export interface GatewayBodyRejectionInput {
   statusCode: number
